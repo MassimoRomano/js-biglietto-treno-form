f9d54844eb9da5749709dac1afd8d3305be7c061
MassimoRomano <romano93m@gmail.com>
2024-02-20 19:09:16 +0100

    add get element

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ console.log("funziona");
 const btnElement = document.querySelector('#btnCalc')
 btnElement.addEventListener('click', function() {
 
-const Name = document.getElementById("userName") ;
+const Name = document.getElementById("userName").value ;
 //take input by usarName class
 
 const Km = document.getElementById("userKm").value ;
@@ -12,33 +12,48 @@ const Km = document.getElementById("userKm").value ;
 const Age = document.getElementById("userAge").value ;
 //take input number by userAge class
 
+const Carrozza = Math.random()* 100
+//const for random number
+
+const CodCP = Math.random()* 100000000
+//const for random CodCP
+
 console.log(Name, Km, Age);
 
 const forKm = 0.21 ;
 //price for k
-const fullPrice = forKm * Km
+const fullPrice = forKm * Km;
 //full price
-const discountMin = fullPrice *  0.2
+const discountMin = fullPrice *  0.2;
 //discount del 20%
-const discountMax = fullPrice *  0.4
+const discountMax = fullPrice *  0.4;
 //discount del 40%
 
 
 if(Age < 18){
-    price = (fullPrice - discountMin).toFixed(2)
+    price = (fullPrice - discountMin).toFixed(2);
 }
 //price for <18
 
 else if (Age > 65){
-    price = (fullPrice - discountMax).toFixed(2)
+    price = (fullPrice - discountMax).toFixed(2);
 }
-//price for =>65
+//price for >65
 
 else{
-    price = fullPrice.toFixed(2)
+    price = fullPrice.toFixed(2);
 }
 // full price
+
+
+
 console.log(`price: ${price} €`);
+//stamp price in console
+
+document.getElementById("ticket-name").innerText = Name;
+document.getElementById("ticket-pos").innerText = Carrozza.toFixed();
+document.getElementById("ticket-codCP").innerText = CodCP.toFixed();
+document.getElementById("ticket-price").innerText = price
 })
 
 
