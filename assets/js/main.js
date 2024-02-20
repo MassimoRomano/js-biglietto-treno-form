e493aaa8c24a6a3e601701951891ce2fff7500fa
MassimoRomano <romano93m@gmail.com>
2024-02-20 16:13:20 +0100

    add button if and else

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,42 @@
-console.log("funziona")
+console.log("funziona");
 
-const forKm = 0.21
-//price for km
-const Name = document.getElementsByClassName("userName")
+const btnElement = document.querySelector('#btnCalc')
+btnElement.addEventListener('click', function() {
+
+const Name = document.getElementsByClassName("userName") ;
 //take input by usarName class
 
-const Km = document.getElementsByClassName("userKm").value
+const Km = document.getElementsByClassName("userKm").value ;
 //take input number by userKm class
 
-const Age = document.getElementsByClassName("userAge").value
+const Age = document.getElementsByClassName("userAge").value ;
 //take input number by userAge class
+
+const forKm = 0.21 ;
+//price for k
+const fullPrice = forKm * Km
+//full price
+const discountMin = fullPrice *  0.2
+//discount del 20%
+const discountMax = fullPrice *  0.4
+//discount del 40%
+
+
+if(Age < 18){
+    price = (fullPrice - discountMin).toFixed(2)
+}
+//price for <18
+
+else if (Age > 65){
+    price = (fullPrice - discountMax).toFixed(2)
+}
+//price for =>65
+
+else{
+    price = fullPrice.toFixed(2)
+}
+// full price
+console.log(Name, Km, Age)
+})
+
+
