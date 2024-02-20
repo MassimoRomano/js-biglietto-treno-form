1bc5876e150d666530012dbc6014b52c08c27482
MassimoRomano <romano93m@gmail.com>
2024-02-20 15:30:20 +0100

    add const km

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 console.log("funziona")
 
 const forKm = 0.21
-
+//price for km
 const Name = document.getElementsByClassName("userName")
+//take input by usarName class
 
-
+const Km = document.getElementsByClassName("userKm").value
+//take input number by userKm class
